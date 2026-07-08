985aaec5933e8c426440483617b0bb10bdfb3f45
uyioriaghan <uyi@kuda.com>
2026-07-08 08:48:55 +0100

    chore: remove stray db files and add to gitignore

--- test.kt
+++
@@ -1,5 +0,0 @@
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Link
-import androidx.compose.material.icons.filled.Public
-import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material.icons.filled.Search
